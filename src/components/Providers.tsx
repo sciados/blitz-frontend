94c1806bf6a32ab59238cc29538264ca79a6faa4
sciados <52825780+sciados@users.noreply.github.com>
2025-12-04 15:04:28 +0000

    feat: Update Sidebar Menus 48.0

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -20,7 +20,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <Toaster position="top-right" richColors />
+      <Toaster position="bottom-right" richColors />
     </QueryClientProvider>
   );
 }
